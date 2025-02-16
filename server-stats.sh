32640fd62cea06b02d94d04c293d6258ffed896b
Stella Bischoff <bischoff.stella3@gmail.com>
2025-02-16 17:07:30 +0000

    added mem usage stats

--- server-stats.sh
+++ server-stats.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # CPU Usage
-cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1 "%"}')
-echo -e "CPU USAGE ${cpu_usage} \n"
+echo "******| CPU     |******"
+read cpu_usage <<< $(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1 "%"}')
+echo -e "CPU Usage(%) ${cpu_usage} \n"
 
 # Total memory usage (Free vs Used including percentage)
+echo "******| MEMORY  |******"
+read mem_free mem_used <<< $(top -bn1 | grep "MiB Mem" | sed "s/.*\([0-9.]*\) total, *\([0-9.]*\) free, *\([0-9.]*\) used.*/\1 \2 \3/")
+echo -e "Memory Free(%) $(awk "BEGIN {print $mem_free/($mem_free+$mem_used) * 100; exit}")"
+echo -e "Memory Used(%) $(awk "BEGIN {print $mem_used/($mem_free+$mem_used) * 100; exit}")"
 
 # Total disk usage (Free vs Used including percentage)
 
